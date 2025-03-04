953a29a38ff7c1121369577da5ebe726a86223d0
VitorVLima <vitor14_df@hotmail.com>
2025-03-04 00:33:44 -0300

    segunda att

--- estudo/view.py
+++ estudo/view.py
@@ -1,7 +1,18 @@
 from estudo import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route("/")
-
 def homepage():
-    return render_template("index.html")
+    usuario = "vitor vinicius"
+    idade = 26
+    cpf = "076466043-80"
+    context = {
+        'usuario': usuario,
+        'idade': idade,
+        'cpf': cpf
+    }
+    return render_template("index.html", context = context)
+
+@app.route("/nova/")
+def novapage():
+    return "nova pagina"
